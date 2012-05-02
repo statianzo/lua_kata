730d86aa25239d47f8aee1e1d70cd24ae060c8e5
Jason Staten <jstaten07@gmail.com>
2012-05-02 00:58:45 -0600

    dual leaf

--- kata.lua
+++ kata.lua
@@ -2,14 +2,54 @@ module('kata', package.seeall)
 
 local kata = {}
 
+function kata.split(path, separator)
+  local output = {}
+  for x in string.gmatch(path, '[^' .. separator .. ']+') do
+    table.insert(output, x)
+  end
+  return output
+end
+
+function kata.combinations(elements, length)
+  local combos = {}
+
+  for i=1, #elements do
+    if length == 1 then
+      return {{elements[i]}}
+    else
+      local subelements = {}
+      for j=2, #elements do
+        table.insert(subelements, elements[j])
+      end
+      for _, n in ipairs(kata.combinations(subelements, length-1)) do
+        return {kata.merge_table({elements[i]}, n)}
+      end
+    end
+  end
+
+  return combos
+end
+
+
+function kata.merge_table(t1, t2)
+  for k,v in pairs(t2) do
+    table.insert(t1, k, v)
+  end
+  return t1
+end
+
 function kata.build_tree(path, existing)
   path = path or ""
 
   existing = existing or {}
   local last = existing
-  for w in string.gmatch(path, '%a+') do
-      last[w] = {}
-      last = last[w]
+  local nodes = kata.split(path, '/')
+  for _, w in ipairs(nodes) do
+    local conodes = kata.split(w, '|')
+    for _, x in ipairs(conodes) do
+      last[x] = {}
+    end
+    last = last[w]
   end
   return existing
 end
--- kata_test.lua
+++ kata_test.lua
@@ -1,7 +1,7 @@
 require 'lunit'
 local kata = require 'kata'
 
-module('kata_test', lunit.testcase)
+module('kata_test', lunit.testcase, package.seeall)
 
 function test_it_builds_tree()
   local result = kata.build_tree()
@@ -27,4 +27,26 @@ function test_adding_to_an_existing_tree()
   local tree = kata.build_tree("/foo/bar")
   local result = kata.build_tree("/foo/baz", tree)
   assert_not_nil(result.foo.baz)
+end
+
+function test_will_insert_dual_leaf_node()
+  local result = kata.build_tree("/foo/bar|baz")
+  assert_table(result.foo.bar)
+  assert_table(result.foo.baz)
+end
+
+function test_adding_to_an_existing_tree()
+  local tree = kata.build_tree("/foo/bar")
+  local result = kata.build_tree("/foo/bar/baz|qux", tree)
+  assert_table(result.foo.bar.baz)
+  assert_table(result.foo.bar.qux)
+end
+
+
+function test_split_will_separate()
+  local result = kata.split("/foo/bar/baz|bing", "/")
+  assert_table(result)
+  assert_equal(result[1], 'foo')
+  assert_equal(result[2], 'bar')
+  assert_equal(result[3], 'baz|bing')
 end
